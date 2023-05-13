e9b0fba0fa152c61a7dd61f07be08f149f53d4ac
Camilo <camilopaezescobar@gmail.com>
2023-05-12 19:41:39 -0500

    logica html de seleccion del enemigo

--- Camilomon (pokemon)/js/pokemon.js
+++ Camilomon (pokemon)/js/pokemon.js
@@ -1,4 +1,7 @@
 // alert("hola!")
+function aletatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
 function seleccionarMascota() {
 
@@ -9,23 +12,53 @@ function seleccionarMascota() {
     let inputGarchomp = document.getElementById("Garchomp")
     let inputKabutops = document.getElementById("Kabutops")
 
+    let spanMascotaJugador = document.getElementById("span_nombre_mascota")
+
 
     if (inputShayim.checked) {
-        alert("Seleccionaste a Shayim!")
+        spanMascotaJugador.innerHTML = "Shayim"
     } else if (inputSunflora.hecked) {
-        alert("Seleccionaste a Sunflora!")
+        spanMascotaJugador.innerHTML = "Sunflora"
     } else if (inputTotodaile.checked) {
-        alert("Seleccionaste a Totodaile!")
+        spanMascotaJugador.innerHTML = "Totodaile"
     } else if (inputCharizard.checked) {
-        alert("Seleccionaste a Charizard!")
+        spanMascotaJugador.innerHTML = "Charizard"
     } else if (inputGarchomp.checked) {
-        alert("Seleccionaste a Garchomp!")
+        spanMascotaJugador.innerHTML = "Garchomp"
     } else if (inputKabutops.checked) {
-        alert("Seleccionaste a Kabutops!")
+        spanMascotaJugador.innerHTML = "Kabutops"
     } else (
     alert ("Selecciona un pokemon!")
     )
+
+    mascotaDelEnemigoRandom ()
+
     // document.getElementsByName('mokepon')
+}
+
+function mascotaDelEnemigoRandom () {
+    let ataqueAleatorio = aletatorio(1,6)
+    let spanMascotaEnemigo = document.getElementById('span_nombre_mascota_enemigo')
+
+    if (ataqueAleatorio == 1) {
+        // Shayim
+        spanMascotaEnemigo.innerHTML = 'Shayim'
+    } else if (ataqueAleatorio == 2) {
+        // Sunflora
+        spanMascotaEnemigo.innerHTML = 'Sunflora'
+    } else if (ataqueAleatorio == 3) {
+        // Totodaile
+        spanMascotaEnemigo.innerHTML = 'Totodaile'
+    } else if (ataqueAleatorio == 4) {
+        // Charizard
+        spanMascotaEnemigo.innerHTML = 'Charizard'
+    } else if (ataqueAleatorio == 5) {
+        // Garchomp
+        spanMascotaEnemigo.innerHTML = 'Garchomp'
+    } else if (ataqueAleatorio == 6) {
+        // Kabutops
+        spanMascotaEnemigo.innerHTML = 'Kabutops'
+    }
 }
 
 function iniciarJuago() {
